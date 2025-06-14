8ce1e89aa5e7d762cb1808e7358550ab29ec0e67
Mohamad Aswad <aswad32@gmail.com>
2025-06-14 22:55:21 +0800

    Update DBConnection.java

--- A1/src/java/util/DBConnection.java
+++ A1/src/java/util/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // newer driver
-            Connection conn = DriverManager.getConnection('jdbc:mysql://mysql-iyu9.railway.internal:3306/railway', 
+            Connection conn = DriverManager.getConnection('jdbc:mysql://mysql-iyu9.railway.internal:3306/railway?sslMode=REQUIRED&allowPublicKeyRetrieval=true&useSSL=true', 
                                               'root', 'ZUSUMHvlcUbhwUjJAMxblpdzTSnluvsC');
             System.out.println(conn);
             return conn;
